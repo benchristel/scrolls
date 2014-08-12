fc6e094e84d60db09866f0a5fc1b0c4194ce964a
Ben Christel <ben.christel@gmail.com>
2014-08-11 21:51:04 -0700

    deprecated installing modules using obj.mod(myModule)
    Lantern still does this under the hood when using myModule(obj), but it's not going to be part of the API.

--- js/spec/lantern-spec.js
+++ js/spec/lantern-spec.js
@@ -32,84 +32,77 @@ describe('Lantern', function() {
                 this.object = Lantern.makeObject()
             })
 
-            it('has an extend method', function() {
-                expect(typeof this.object.mod).toEqual('function')
+            it('has a mod method', function() {
+                expect(typeof Lantern.makeObject().mod).toEqual('function')
+            })
+        })
+    })
+
+    describe('Lantern::Object', function() {
+        beforeEach(function() {
+            this.object = Lantern.makeObject()
+        })
+
+        describe('.mod', function() {
+            it('modifies the original object', function() {
+                this.object.mod(function(self) {
+                    self.foo = 'bar'
+                })
+
+                expect(this.object.foo).toEqual('bar')
             })
 
-            describe('extending the created object', function() {
-                it('modifies the original object', function() {
-                    this.object.mod(function(self) {
-                        self.foo = 'bar'
-                    })
-
-                    expect(this.object.foo).toEqual('bar')
+            it('returns the original object', function() {
+                var returned = this.object.mod(function(self) {
+                    self.foo = 'bar'
                 })
 
-                it('returns the original object', function() {
-                    var returned = this.object.mod(function(self) {
-                        self.foo = 'bar'
-                    })
-
-                    expect(returned).toBe(this.object)
-                })
-
-                it('an object cannot extend a module more than once', function() {
-                    var module = function(self) {
-                        self.foo = 'bar'
-                    }
-
-                    this.object.mod(module)
-                    expect(this.object.foo).toEqual('bar')
-                    this.object.foo = 'quaxxor'
-                    this.object.mod(module)
-                    expect(this.object.foo).toEqual('quaxxor')
-                })
-
-                it('passes the object, private container, super of the object, and super of the private container to the module function', function() {
-                    this.object.mod(function(self, _self, original, _original) {
-                        self.getSecret = function() { return _self.secret }
-                        _self.secret = 'be sure to drink your ovaltine'
-                    })
-
-                    expect(this.object.secret).toBe(undefined)
-                    expect(this.object.getSecret()).toBe('be sure to drink your ovaltine')
-                })
-
-                it('allows methods defined by the module to call super methods', function() {
-                    this.object.mod(function(self, _self) {
-                        self.getSecret = function() { return "From the depths of my soul: " + _self.getSecret() }
-                        _self.getSecret = function() { return _self.secret }
-                        _self.secret = 'be sure to drink your ovaltine'
-                    })
-
-                    expect(this.object.secret).toBe(undefined)
-                    expect(this.object.getSecret()).toEqual('From the depths of my soul: be sure to drink your ovaltine')
-
-                    this.object.mod(function(self, _self, sup, _sup) {
-                        self.getSecret = function() {
-                            return 'From ages past: ' + sup.getSecret()
-                        }
-
-                        _self.getSecret = function() {
-                            return _sup.getSecret().toUpperCase()
-                        }
-                    })
-
-                    expect(this.object.getSecret()).toEqual('From ages past: From the depths of my soul: BE SURE TO DRINK YOUR OVALTINE')
-                })
+                expect(returned).toBe(this.object)
             })
 
-            describe('extending with multiple module definitions', function() {
-                it('calls the modules in order', function() {
-                    var makeFoo = function(self) {
-                        self.foo = function() { return "foo" }
-                    }
-                    var makeBar = function(self, _, sup) {
-                        self.foo = function() { return sup.foo() + 'bar' }
+            it('does not allow modification by the same function more than once', function() {
+                var setFoo = function(self) {
+                    self.foo = 'bar'
+                }
+
+                this.object.mod(setFoo)
+                expect(this.object.foo).toEqual('bar')
+                this.object.foo = 'quaxxor'
+                this.object.mod(setFoo)
+                expect(this.object.foo).toEqual('quaxxor')
+            })
+
+            it('passes the object, private container, super of the object, and super of the private container to the module function', function() {
+                this.object.mod(function(self, _self, original, _original) {
+                    self.getSecret = function() { return _self.secret }
+                    _self.secret = 'be sure to drink your ovaltine'
+                })
+
+                expect(this.object.secret).toBe(undefined)
+                expect(this.object.getSecret()).toBe('be sure to drink your ovaltine')
+            })
+
+            it('allows methods defined in the function passed to mod to call super methods', function() {
+                this.object.mod(function(self, _self) {
+                    self.getSecret = function() { return "From the depths of my soul: " + _self.getSecret() }
+                    _self.getSecret = function() { return _self.secret }
+                    _self.secret = 'be sure to drink your ovaltine'
+                })
+
+                expect(this.object.secret).toBe(undefined)
+                expect(this.object.getSecret()).toEqual('From the depths of my soul: be sure to drink your ovaltine')
+
+                this.object.mod(function(self, _self, sup, _sup) {
+                    self.getSecret = function() {
+                        return 'From ages past: ' + sup.getSecret()
                     }
 
-                    expect(this.object.mod(makeFoo, makeBar).foo()).toEqual('foobar')
+                    _self.getSecret = function() {
+                        return _sup.getSecret().toUpperCase()
+                    }
                 })
+
+                expect(this.object.getSecret()).toEqual('From ages past: From the depths of my soul: BE SURE TO DRINK YOUR OVALTINE')
             })
         })
     })
@@ -123,63 +116,85 @@ describe('Lantern', function() {
             this.obj = Lantern.makeObject()
         })
 
-        it('given an extension function, returns a function that, given an object, extends the object with the extension function', function() {
+        it('given a mod definition, returns a function that, given an object, mods the object', function() {
             expect(this.obj.foo).toBe(undefined)
             this.setFoo(this.obj)
             expect(this.obj.foo).toBe(1)
         })
 
-        it('given multiple extension functions, returns a function that, given an object, extends the object with each extension function in order', function() {
-            var setFooAndBar = Lantern.createModule(
-                function(target) {
-                    target.foo = 1
-                    target.baz = []
-                },
-                function(target) {
-                    target.bar = 2
-                    target.baz.push(1)
+        it('can define a module with dependencies on other modules', function() {
+            var defineFoo = Lantern.createModule(function(target, _target) {
+                _target.foo = function() { return 1 }
+            })
+
+            var publicizeFoo = Lantern.createModule(
+                defineFoo,
+                function(target, _target, sup, _sup) {
+                    target.foo = function() { return "Foo is " + _sup.foo() }
                 }
             )
 
-            expect(this.obj.foo).toBe(undefined)
+            publicizeFoo(this.obj)
 
-            setFooAndBar(this.obj)
-
-            expect(this.obj.foo).toBe(1)
-            expect(this.obj.bar).toBe(2)
-            expect(this.obj.baz).toEqual([1])
+            expect(this.obj.foo()).toEqual("Foo is 1")
         })
 
-        describe('the returned function', function() {
-            it('returns the extended object', function() {
-                expect(this.setFoo(this.obj)).toBe(this.obj)
+        it('does not re-add dependencies if the object has already installed them', function() {
+            var defineFoo = Lantern.createModule(function(target) {
+                target.fooCounter++
             })
 
-            it('creates a new object if given none', function() {
-                var obj = this.setFoo()
-                expect(obj.foo).toBe(1)
+            var obj = {fooCounter: 0}
+            defineFoo(obj)
+            expect(obj.fooCounter).toBe(1)
+
+            var publicizeFoo = Lantern.createModule(
+                defineFoo, function() {}
+            )
+
+            publicizeFoo(obj)
+
+            expect(obj.fooCounter).toBe(1)
+        })
+    })
+
+    describe('a Lantern module', function() {
+        beforeEach(function() {
+            this.f = function(target) {
+                target.foo = 1
+            }
+            this.setFoo = Lantern.createModule(this.f)
+            this.obj = Lantern.makeObject()
+        })
+
+        it('returns the modded object', function() {
+            expect(this.setFoo(this.obj)).toBe(this.obj)
+        })
+
+        it('creates a new object if given none', function() {
+            var obj = this.setFoo()
+            expect(obj.foo).toBe(1)
+        })
+
+        it('defines the mod method if given an object that does not implement it', function() {
+            var obj = this.setFoo({})
+            expect(obj.mod).not.toBe(undefined)
+        })
+
+        it('can be passed back to .createModule to create compound modules', function() {
+            var makeFoo = Lantern.createModule(function(target, _target) {
+                target.foo = function() { return _target.foo }
             })
-
-            it('defines the extend method if given an object that does not implement it', function() {
-                var obj = this.setFoo({})
-                expect(obj.mod).not.toBe(undefined)
+            var makeBar = Lantern.createModule(function(target, _target, sup) {
+                target.bar = function() { return sup.foo() + 1 }
+                _target.foo = 1
             })
+            var makeFooAndBar = Lantern.createModule(makeFoo, makeBar)
 
-            it('can be passed back to .createModule to create compound modules', function() {
-                var makeFoo = Lantern.createModule(function(target, _target) {
-                    target.foo = function() { return _target.foo }
-                })
-                var makeBar = Lantern.createModule(function(target, _target, sup) {
-                    target.bar = function() { return sup.foo() + 1 }
-                    _target.foo = 1
-                })
-                var makeFooAndBar = Lantern.createModule(makeFoo, makeBar)
+            var obj = makeFooAndBar()
 
-                var obj = makeFooAndBar()
-
-                expect(obj.foo()).toEqual(1)
-                expect(obj.bar()).toEqual(2)
-            })
+            expect(obj.foo()).toEqual(1)
+            expect(obj.bar()).toEqual(2)
         })
     })
 
